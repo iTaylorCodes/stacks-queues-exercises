f6d71d4c8f7aa2c408afccbc05dc3c63bca4fe0f
IanTaylor <76837421+iTaylorCodes@users.noreply.github.com>
2021-11-06 20:42:16 -0700

    Created classes for stacks and queues

---
+++ queue.js
@@ -0,0 +1,60 @@
+/** Node: node for a queue. */
+
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+/** Queue: chained-together nodes where you can
+ *  remove from the front or add to the back. */
+
+class Queue {
+  constructor() {
+    this.first = null;
+    this.last = null;
+    this.size = 0;
+  }
+
+  /** enqueue(val): add new value to end of the queue. Returns undefined. */
+
+  enqueue(val) {
+    const newNode = new Node(val);
+
+    this.last = newNode;
+    this.size++;
+    if (!this.first) this.first = this.last;
+  }
+
+  /** dequeue(): remove the node from the start of the queue
+   * and return its value. Should throw an error if the queue is empty. */
+
+  dequeue() {
+    if (this.size === 0) throw new Error("Queue is empty.");
+
+    const temp = this.first.val;
+    this.first.next = this.first;
+    this.size--;
+
+    return temp;
+  }
+
+  /** peek(): return the value of the first node in the queue. */
+
+  peek() {
+    if (!this.first) {
+      throw new Error("Queue is empty.");
+    } else {
+      return this.first.val;
+    }
+  }
+
+  /** isEmpty(): return true if the queue is empty, otherwise false */
+
+  isEmpty() {
+    return this.size === 0;
+  }
+}
+
+module.exports = Queue;
